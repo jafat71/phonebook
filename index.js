b72f506e3d5748e0043a8f7227376dea89068573
jzafvt771 <ijmm7171@gmail.com>
2023-10-11 03:25:02 -0500

    add dist

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :r
 
 const cors = require('cors')
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = require('./personsDB.js')
 port = process.env.PORT || 3001
